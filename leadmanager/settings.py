4cba6923cc31ccc712bb3418bfc2c43054069b61
zolomohan <mohanzolomon@gmail.com>
2020-04-23 17:12:54 +0530

    Register Frontend App to settings.py

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
-    'rest_framework'
+    'frontend',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
